501c6dd1618e2f1da75da93947edf39f0863d59e
jimzer <jimi.vaubien@protonmail.com>
2022-08-05 23:19:53 +0200

    Fix sitemap to work on cloudflare

--- src/routes/sitemap.xml.ts
+++ src/routes/sitemap.xml.ts
@@ -1,37 +1,50 @@
-import { CONFIG } from "@config"
-import { getAllArticlesMeta } from "@lib/cms"
-import { SitemapStream, streamToPromise } from "sitemap"
+import { CONFIG } from '@config'
+import { getAllArticlesMeta } from '@lib/cms'
 
+function xmlUrlBlock(url: string) {
+	return `
+	<url>
+		<loc>${url}</loc>
+		<changefreq>daily</changefreq>
+		<priority>0.3</priority>
+	</url>
 
+  `
+}
 
 export async function get() {
-    const headers = {
-      'Cache-Control': 'max-age=0, s-maxage=3600',
-      'Content-Type': 'application/xml',
-    }
+	const headers = {
+		'Cache-Control': 'max-age=0, s-maxage=3600',
+		'Content-Type': 'application/xml'
+	}
 
-    const articles = await getAllArticlesMeta(100)
-    const slugs = articles.map(article => article.attributes.slug)
+	const articles = await getAllArticlesMeta(100)
+	const slugs = articles.map((article) => article.attributes.slug)
 
-    const smStream = new SitemapStream({ hostname: CONFIG.seo.publicUrl})
+	let body = ''
 
+	body += xmlUrlBlock(CONFIG.seo.publicUrl + '/')
+	body += xmlUrlBlock(CONFIG.seo.publicUrl + '/blog')
+	body += xmlUrlBlock(CONFIG.seo.publicUrl + '/about')
 
-    smStream.write({ url: '/',  changefreq: 'daily', priority: 0.3 })
-    smStream.write({ url: '/blog',  changefreq: 'daily', priority: 0.3 })
-    smStream.write({ url: '/about',  changefreq: 'daily', priority: 0.3 })
+	slugs.forEach((slug: string) => {
+		body += xmlUrlBlock(CONFIG.seo.publicUrl + '/blog/' + slug)
+	})
 
-    slugs.forEach((slug: string) => {
-        smStream.write({ url: `/blog/${slug}`,  changefreq: 'daily', priority: 0.3 })
-    })
+	const sitemap = `
+<urlset
+	xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+	xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
+	xmlns:xhtml="http://www.w3.org/1999/xhtml"
+	xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
+	xmlns:video="http://www.google.com/schemas/sitemap-video/1.1"
+>
+${body}
+</urlset>
+`
 
-    smStream.end()
-
-    const sm = (await streamToPromise(smStream)).toString()
-
-    return {
-        headers,
-        body: sm
-    }
-
-    
-  }
+	return {
+		headers,
+		body: sitemap
+	}
+}
